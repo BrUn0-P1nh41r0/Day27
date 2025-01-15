6010c1be37614b2e76e55b8d8143dade40b39574
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-01-15 12:24:15 +0000

    updated day 27

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import tkinter
 
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height= 300)
@@ -7,6 +12,16 @@ window.minsize(width=500, height= 300)
 #label
 my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+#button
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+#Entry
+input = tkinter.Entry(width=10)
+input.pack()
+
 
 
 window.mainloop()
